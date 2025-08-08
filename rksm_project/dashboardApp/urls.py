cb4267d6174c8bc0d4ad841e6fcd22d878a65fa6
Shviam Shode <shivam.shode@hiveminds.com>
2025-08-08 16:47:38 +0530

    more paths added

--- rksm_project/dashboardApp/urls.py
+++ rksm_project/dashboardApp/urls.py
@@ -3,5 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-   path('form/', views.req),
+    path('forms/', views.req),
+    path('admin/', admin.site.urls),
+    path('home/', views.req),
+    path('tq/', views.req),
+    path('graph/', views.req),
 ]
