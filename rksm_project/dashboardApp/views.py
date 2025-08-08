3638f84c6925d1f38e79abf3ca0908ad5345208e
Shviam Shode <shivam.shode@hiveminds.com>
2025-08-08 16:19:24 +0530

    second commit

--- rksm_project/dashboardApp/views.py
+++ rksm_project/dashboardApp/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from .models import Student
+
+#git push -u origin shivam
 
 def req(request):
+    if request.method=='POST':
+        studentName=request.POST.get('studentName')
+        marks=request.POST.get('marks')
+        subject=request.POST.get('subject')
+        if name and email and message:
+            Contact.objects.create(name=name,email=email,message=message)
+            return render(request,'thankyou.html')
     return render(request, 'forms.html')
