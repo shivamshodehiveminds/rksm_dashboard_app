e18482f142874eaa2c35af7ab734c2126a362875
Richa <richa.kumari@hiveminds.in>
2025-08-08 16:34:24 +0530

    xyz

--- rksm_project/dashboardApp/models.py
+++ rksm_project/dashboardApp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+    
+class Student(models.Model):
+    studentName = models.CharField(max_length = 100)
+    marks = models.CharField(max_length = 100)
+    subject = models.CharField(max_length = 100)
+
 # Create your models here.
